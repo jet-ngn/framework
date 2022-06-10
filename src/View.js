5fa6d51258569a0f7f0143b1d18352f3f00f5be7
Graham Butler <graham@grahambutler.com>
2022-06-09 17:05:08 -0700

    remove commented code

--- src/View.js
+++ src/View.js
@@ -1,16 +1,11 @@
 import Entity from './Entity'
-// import Permissions from './Permissions'
 
 export default class View extends Entity {
   #route
 
-  constructor (parent, rootNode, { permissions }, route) {
-    super(parent, rootNode, arguments[2], 'view')
+  constructor (parent, rootNode, config, route) {
+    super(parent, rootNode, config, 'view')
     this.#route = route ?? null
-
-    // if (!!permissions) {
-    //   Permissions.addView(this, permissions)
-    // }
   }
 
   get route () {
